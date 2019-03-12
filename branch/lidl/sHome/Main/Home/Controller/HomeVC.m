41bb27dda4f2cd9b191dbd56ed79aca4d52bac0e
Ryan Hsueh <freestyle0103@gmail.com>
2019-03-12 11:52:42 +0800

    [What] Show message when user want to change scene mode but gateway is offline

--- branch/lidl/sHome/Main/Home/Controller/HomeVC.m
+++ branch/lidl/sHome/Main/Home/Controller/HomeVC.m
@@ -429,9 +429,16 @@ BOOL flag_checkfireware = NO;
     NSUserDefaults *config = [NSUserDefaults standardUserDefaults];
     DeviceListModel *model = [[DeviceListModel alloc] initWithDictionary:[config objectForKey:DeviceInfo] error:nil];
     
-    if (model != nil) {
-        _menuVc.view.hidden = NO;
-        [_menuVc open];
+    if (model != nil && [model.online isEqualToString:@"1"]) {
+        NSUserDefaults *config = [NSUserDefaults standardUserDefaults];
+        DeviceListModel *model = [[DeviceListModel alloc] initWithDictionary:[config objectForKey:DeviceInfo] error:nil];
+        
+        if (model != nil) {
+            _menuVc.view.hidden = NO;
+            [_menuVc open];
+        }
+    } else {
+        [MBProgressHUD showSuccess:[NSString stringWithFormat:NSLocalizedString(@"当前网关为:%@", nil),NSLocalizedString(@"离线", nil) ] ToView:self.view];
     }
 }
 
