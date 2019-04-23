7c581c5c94364c841a5d0f4a25de81937a79a254
Ryan Hsueh <freestyle0103@gmail.com>
2019-04-23 17:34:10 +0800

    [What] Updated switch device status green for "ON", red for "OFF"

--- branch/lidl/sHome/Main/Device/Controller/DeviceDetailVC.m
+++ branch/lidl/sHome/Main/Device/Controller/DeviceDetailVC.m
@@ -485,7 +485,7 @@
         [_bgImageView setImage:[UIImage imageNamed:@"sbgreen_bg"]];
         if ([_data.title isEqualToString:@"智能插座"]) {
             _MainLabel.text = NSLocalizedString(@"Socket OFF",nil);
-            [_deviceSwitch setOn:NO animated:YES];
+            [_deviceSwitch setOn:YES animated:YES];
         }
         else if([_data.title isEqualToString:@"门磁"]){
             _MainLabel.text = NSLocalizedString(@"门关闭",nil);
@@ -555,7 +555,7 @@
         if ([_data.title isEqualToString:@"智能插座"]) {
             [_bgImageView setImage:[UIImage imageNamed:@"sbred_bg"]];//
             _MainLabel.text = NSLocalizedString(@"Socket ON",nil);
-            [_deviceSwitch setOn:YES animated:YES];
+            [_deviceSwitch setOn:NO animated:YES];
         }
         else if([_data.title isEqualToString:@"门磁"]){
             [_bgImageView setImage:[UIImage imageNamed:@"sbred_bg"]];//
