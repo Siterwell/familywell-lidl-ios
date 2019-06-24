11d69e78be09b450e80c9290ce1df67e58f2ec86
Ryan Hsueh <freestyle0103@gmail.com>
2019-06-24 16:17:27 +0800

    [What] Added TempControlDetailVC2

---
+++ branch/lidl/sHome/Main/Device/Controller/TempControlDetailVC2.h
@@ -0,0 +1,21 @@
+//
+//  TempControlDetailVC2.h
+//  sHome
+//
+//  Created by tracyhenry on 2018/11/8.
+//  Copyright © 2018 shaop. All rights reserved.
+//
+
+#ifndef TempControlDetailVC2_h
+#define TempControlDetailVC2_h
+#import <UIKit/UIKit.h>
+#import "BaseVC.h"
+#import "ItemData.h"
+
+@interface TempControlDetailVC2 : BaseVC
+
+@property (nonatomic, strong) ItemData *data;
+
+@end
+
+#endif /* TempControlDetailVC2_h */
